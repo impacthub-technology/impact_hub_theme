4df457eadcd4d56a8d5f7ea699aa61e9c593d8af
Rodrigo Cauduro <rodrigo.cauduro@impacthub.net>
2017-12-01 17:25:45 +0100

    change update version

--- inc/theme-init.php
+++ inc/theme-init.php
@@ -24,9 +24,10 @@ class socialLinks extends WP_Widget {
 	function widget( $args, $instance ) {
 		echo getSocial();
 	}
-	function form () {}
-	function update() {}
+	function form ( $instance ) {}
+	function update($new_instance, $old_instance) {}
 }
+
 class siteLogo extends WP_Widget {
 	function __construct() {
 		parent::__construct( 'siteLogo', 'Logo' );
@@ -34,8 +35,8 @@ class siteLogo extends WP_Widget {
 	public function widget( $args, $instance ) {
 		echo getLogo( false );
 	}
-	function form () {}
-	function update() {}
+	function form ( $instance ) {}
+	function update( $new_instance, $old_instance ) {}
 
 }
 
