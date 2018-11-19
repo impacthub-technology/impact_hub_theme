66202b2e5fc782be41676a51a777f7fa0e7b2fa0
Honza Kopecký <honza.kopecky95@gmail.com>
2018-11-19 22:45:16 +0100

    Fix bug #6 & improve single-space.php code

--- single-space.php
+++ single-space.php
@@ -1,69 +1,50 @@
 <?php get_header(); ?>
 
-<div class="container">
-  <?php while ( have_posts() ) { the_post(); ?>
-    <div class="container area-stories">
-        <div id="sTitle">
-            <div class="icon" style="background-image:url(<?php echo get_the_post_thumbnail_url( get_the_ID(), 'full' );  ?>)"></div>
-            <div class="data">
-                <div class="name"><?php echo get_the_title(); ?><div class="bg1"></div></div>
-                <div class="meta bgc2"></div>
+    <div class="container">
+		<?php while ( have_posts() ) : the_post(); ?>
+            <div class="container area-stories">
+                <div id="sTitle">
+                    <div class="icon" style="background-image:url(<?php echo get_the_post_thumbnail_url( get_the_ID(), 'full' );  ?>)"></div>
+                    <div class="data">
+                        <div class="name"><?= get_the_title(); ?><div class="bg1"></div></div>
+                        <div class="meta bgc2"></div>
+                    </div>
+                </div>
             </div>
-        </div>
+
+            <div id="single">
+                <div class="container">
+                    <p><?= get_the_content(); ?></p>
+
+                    <ul class="list-unstyled">
+                        <li><?= __('Size', 'impact-hub-theme') ?> <?= get_field( "room_size" ) ?> <?= get_field( "room_size_unit" ) ?></li>
+                        <li><?= __('Seating', 'impact-hub-theme') ?> <?= get_field( "seating_capacity" ) ?> <?= get_field( "seating_unit" ) ?></li>
+                        <li><?= __('Standing', 'impact-hub-theme') ?> <?= get_field( "standing_capacity" ) ?> <?= get_field( "standing_unit" ) ?></li>
+                    </ul>
+
+					<?php if ( have_rows('amenities') ): ?>
+                        <h5><?= __('Amenities','impact-hub-theme') ?></h5>
+                        <ul>
+							<?php while ( have_rows('amenities') ) : the_row(); ?>
+                                <li><?= get_sub_field('amenity_title') ?></li>
+							<?php endwhile; ?>
+                        </ul>
+					<?php endif; ?>
+
+					<?php
+					$form_object = get_field('gravity_forms');
+					if($form_object) {
+						gravity_form_enqueue_scripts( $form_object['id'], true );
+						gravity_form( $form_object['id'], true, true, false, '', true, 1 );
+					}
+					?>
+                </div>
+            </div>
+		<?php endwhile; ?>
     </div>
-
-	 <div id="single">
-        <div class="container">
-            <!-- <h5><?php echo get_the_title(); ?></h5> -->
-            <p><?php echo get_the_content(); ?></p>
-
-            <ul class="list-unstyled">
-                <li>Size <?php the_field( "room_size" ); ?> <?php the_field( "room_size_unit" ); ?></li>
-                <li>Seating  <?php the_field( "seating_capacity" ); ?> <?php the_field( "seating_unit" ); ?></li>
-                <li>Standing  <?php the_field( "standing_capacity" ); ?> <?php the_field( "standing_unit" ); ?></li>
-            </ul>
-
-                <?php
-                // check if the repeater field has rows of data
-                if( have_rows('amenities') ):
-                    echo '<h5>Amenities</h5>';
-                    echo '<ul>';
-                    while ( have_rows('amenities') ) : the_row();
-                        // display a sub field value
-                        echo '<li>';
-                        the_sub_field('amenity_title');
-                        echo '</li>';
-                    endwhile;
-                    echo '</ul>';
-                else :
-                    // no rows found
-                endif;
-                ?>
-                <?php
-                // if( have_rows('gravity_forms') ):
-                  // echo '<h5>Book Now</h5>';
-
-                  $form_object = get_field('gravity_forms');
-                  gravity_form_enqueue_scripts( $form_object['id'], true );
-                  gravity_form( $form_object['id'], true, true, false, '', true, 1 );
-                // endif;
-                ?>
-
-        </div>
-    </div>
-
     <style>
         #single h5, #single a {color:#075A61}
         .area-stories .arrow-l {stroke:#075A61!important}
     </style>
-
-
-
-
-
-
-
-  <?php } ?>
-</div>
 
 <?php get_footer(); ?>
